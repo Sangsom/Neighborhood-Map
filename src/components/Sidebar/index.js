93be1513dbf9d0c6b6801bc1e9b6f70ddabc450c
Rinalds Pudulis <rpudulis@gmail.com>
2018-05-18 10:40:39 +0300

    style: Side bar has a vertical scroll

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.aside``;
+const Wrapper = styled.aside`
+  height: 100vh;
+  overflow-y: scroll;
+`;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+const ListItem = styled.li`
+  padding: 20px 15px;
+  background: #ccc;
+  border-bottom: 1px solid black;
+
+  &:hover {
+    background: #ddd;
+    cursor: pointer;
+  }
+`;
 
 class Sidebar extends Component {
   render() {
@@ -12,13 +30,14 @@ class Sidebar extends Component {
         <h2>Sidebar</h2>
         <button onClick={this.props.showMarkers}>Show</button>
         <button onClick={this.props.hideMarkers}>Hide</button>
-        <ul>
+        <input type="text" placeholder="Search" />
+        <List>
           {markers.map((marker, index) => (
-            <li key={index} onClick={() => openInfoWindow(marker)}>
+            <ListItem key={index} onClick={() => openInfoWindow(marker)}>
               {marker.title}
-            </li>
+            </ListItem>
           ))}
-        </ul>
+        </List>
       </Wrapper>
     );
   }
